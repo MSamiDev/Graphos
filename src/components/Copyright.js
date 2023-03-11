4f0430fe8628b1b8f493136d7d8a583d8a224014
Pranshu321 <pranshujain0111@gmail.com>
2023-03-12 01:31:38 +0530

    Copyright error

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import React from "react";
-=======
-import React from 'react';
-import { Link } from 'react-router-dom';
->>>>>>> 7ee4349dbe73d137c913237e1965a72e5bf26a90
+import { Link } from "react-router-dom";
 
 // import copyright data
 import { copyrightData } from "../data";
@@ -18,19 +14,13 @@ const Copyright = () => {
           {/* text */}
           <div className="text-sm font-light italic max-w-[360px]">{text}</div>
           {/* icon */}
-<<<<<<< HEAD
           <div className="-order-1 md:order-1">
             <div className="w-[60px] h-[60px] flex items-center justify-center rounded-full bg-accent-primary/20 cursor-pointer group">
-              <div className="text-3xl text-accent-primary group-hover:scale-110 transition-all">
-=======
-          <div className='-order-1 md:order-1'>
-            <div className='w-[60px] h-[60px] flex items-center justify-center rounded-full bg-accent-primary/20 cursor-pointer group'>
-              <a href="/qna/#top">
-              <div className='text-3xl text-accent-primary group-hover:scale-110 transition-all'>
->>>>>>> 7ee4349dbe73d137c913237e1965a72e5bf26a90
-                {icon}
-              </div>
-              </a>
+              <Link href="/qna/#top">
+                <div className="text-3xl text-accent-primary group-hover:scale-110 transition-all">
+                  {icon}
+                </div>
+              </Link>
             </div>
           </div>
         </div>
